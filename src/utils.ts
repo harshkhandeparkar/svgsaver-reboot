8d115b339fe7d9137c81459a868b328b76b6ed32
HarshKhandeparkar <shadowwarriorpro3003@gmail.com>
2021-10-25 18:54:35 +0530

    feat: clean up utils.ts

--- src/utils.ts
+++ src/utils.ts
@@ -1,11 +1,11 @@
 /* Some simple utilities */
 
-export const isFunction = (a) => typeof a === 'function';
-export const isDefined = (a) => typeof a !== 'undefined';
-export const isUndefined = (a) => typeof a === 'undefined';
-export const isObject = (a) => (a !== null && typeof a === 'object');
+export const isFunction = (a: any) => typeof a === 'function';
+export const isDefined = (a: any) => typeof a !== 'undefined';
+export const isUndefined = (a: any) => typeof a === 'undefined';
+export const isObject = (a: any) => (a !== null && typeof a === 'object');
 
-export function clone (obj) {
+export function clone (obj: Object) {
   if (obj == null || typeof obj !== 'object') { return obj; }
   const copy = obj.constructor();
   for (const attr in obj) {
@@ -15,7 +15,7 @@ export function clone (obj) {
 }
 
 // from https://github.com/npm-dom/is-dom/blob/master/index.js
-export function isNode (val) {
+export function isNode(val: any) {
   if (!isObject(val)) { return false; }
   if (isDefined(window) && isObject(window.Node)) { return val instanceof window.Node; }
   return typeof val.nodeType === 'number' && typeof val.nodeName === 'string';
